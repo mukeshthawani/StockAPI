202594d044576876d702d9a195a3a446ee2278f5
Mukesh Thawani <mukesh9039@gmail.com>
2015-01-23 19:47:50 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name= 'StockAPI',
+    version = '0.0.1',
+    description = 'It is script to extract stock data from rediff money',
+    author = 'Mukesh Thawani',
+    author_email = 'mukesh9039@gmail.com',
+    url = 'https://github.com/mukeshthawani/StockAPI',
+    packages = ['StockAPI'],
+)
